952684f1dc1fdf2cc8b090d759140855895d3587
Android-JCPP-Servo <astewart1138@gmail.com>
2023-02-07 14:08:19 -0600

    Add files for displaying journal

---
+++ jsx_style.js
@@ -0,0 +1,15 @@
+/*********************************
+ * This is to show the difference 
+ * between DOM-created elements 
+ * and JSX-style elements
+ *********************************/
+window.addEventListener('DOMContentLoaded', () => {
+    // Get the host DIVs
+    let domStyle = document.getElementById('domStyle');
+    let jsxStyle = document.getElementById('jsxStyle');
+    
+    //
+    // DOM-Created Elements
+    //
+    let 
+})
